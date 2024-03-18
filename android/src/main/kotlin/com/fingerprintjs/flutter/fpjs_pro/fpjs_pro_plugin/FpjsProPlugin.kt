3a3cad1e34da4b7362299b1c9236bc078a2a237e
ndungudedan <dnkibere@gmail.com>
2024-03-18 18:01:58 +0300

    Bug fix

--- android/src/main/kotlin/com/fingerprintjs/flutter/fpjs_pro/fpjs_pro_plugin/FpjsProPlugin.kt
+++ android/src/main/kotlin/com/fingerprintjs/flutter/fpjs_pro/fpjs_pro_plugin/FpjsProPlugin.kt
@@ -7,7 +7,7 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-
+import io.flutter.plugin.common.MethodChannel.Result
 
 /** FpjsProPlugin */
 class FpjsProPlugin : FlutterPlugin, MethodCallHandler {
